8163a14509873fdbf319fc58033ac7dff32337d8
anagtz82 <anagtz82@me.com>
2018-02-09 16:30:40 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
+// import 'isomorphic-fetch';
+// import 'es6-promise';
+
+// class App extends  Component {
+
+//     componentDidMount() {
+//         fetch("https://ghibliapi.herokuapp.com/films")
+//         .then(res => res.json())
+//         .then(obj => conosole.log(obj));
+//     }
+// render() {
+//     return (
+//         <div className="App">
+// >           <header className="App-header">
+//             <h1 className="App-title">Intro to API</h1>
+//             </header>
+
+//         </div>
+//     )
+// }
+
+
+
+
+// }
+// export default App;
+// ReactDOM.render(<App />, document.getElementById('root'));
+
