ae2c30aaa423b4ba56f7d229948d67bd4dc728e1
anagtz82 <anagtz82@me.com>
2018-02-09 16:59:12 -0600

    added correct import to index.js file

--- src/component.js
+++ src/component.js
@@ -1,8 +1,22 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
+import 'isomorphic-fetch';
+import 'es6-promise';
 
-// class App extends Component {
 
-    
-// }
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
 
-// export default App;
+export default App;
